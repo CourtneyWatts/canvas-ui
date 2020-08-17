2ba03d371ceab882fd115463a854cafaf06f171d
CourtneyWatts <courtney.watts@icloudm>
2020-08-17 21:51:55 +0100

    Updates

---
+++ precache-manifest.ccb54863940eca90333853a19bc7d64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebddb8986ebdda6966d99ac9a8ca6f1b",
+    "url": "/canvas-ui/index.html"
+  },
+  {
+    "revision": "2cb75b66587c8e498c64",
+    "url": "/canvas-ui/static/css/main.0ec294e8.chunk.css"
+  },
+  {
+    "revision": "553af147fcda681fbe25",
+    "url": "/canvas-ui/static/js/2.4f730744.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvas-ui/static/js/2.4f730744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb75b66587c8e498c64",
+    "url": "/canvas-ui/static/js/main.db0c241c.chunk.js"
+  },
+  {
+    "revision": "ed0deec3724bb8c900db",
+    "url": "/canvas-ui/static/js/runtime-main.57ebe8e6.js"
+  }
+]);
